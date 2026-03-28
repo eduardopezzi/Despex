054c62e7f83eb6b8907cb29c4e12caefa6dc48fc
Rui Alves <alvesrui951@gmail.com>
2026-03-28 16:53:14 +0000

    OPR-1: Fix client tests

---
+++ client/tests/app.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from '@app/app';
+
+describe('App Spec', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
