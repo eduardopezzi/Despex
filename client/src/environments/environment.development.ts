0316310d7f84d7f979b624b2a6fba12e291b8af2
Eduardo Pezzi <eduardopezzi@me.com>
2026-06-05 11:34:08 -0300

    fix: complete translations and dev API port

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: `http://${typeof window !== 'undefined' ? window.location.hostname : 'localhost'}:9999/api`,
+  apiUrl: `http://${typeof window !== 'undefined' ? window.location.hostname : 'localhost'}:9191/api`,
 };
